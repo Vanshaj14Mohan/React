194e38e1c5d8605c6271c14bb089a2112ec626d8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-04 23:19:34 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+// function App() {
+//   const name = "Vanshaj"
+//     return( <div className="App">
+//      <h1>{name}</h1>
+//      <h1>{name}</h1>
+//      <h1>{name}</h1>
+//      </div>
+//   );
+// }
+
 function App() {
-  return (
-    <div className="App">Hello World</div>
+  const name = <h1>Vanshaj</h1>
+  const age = <h2>21</h2>
+  const email = <h2>Abc@gmail.com</h2>
+  const user = <div>
+  {name}
+  {age}
+  {email}
+</div>
+    return( <div className="App">
+    {user}
+    {user}
+     </div>
   );
 }
+
 
 export default App;
