b43b4feab74eccfc9b0100421ffcd4f9ae283239
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-05 16:11:00 +0530

    components

--- src/App.js
+++ src/App.js
@@ -11,20 +11,32 @@ import './App.css';
 // }
 
 function App() {
-  const name = <h1>Vanshaj</h1>
-  const age = <h2>21</h2>
-  const email = <h2>Abc@gmail.com</h2>
-  const user = <div>
-  {name}
-  {age}
-  {email}
-</div>
+//   const name = <h1>Vanshaj</h1>
+//   const age = <h2>21</h2>
+//   const email = <h2>Abc@gmail.com</h2>
+//   const user = ( 
+//   <div>
+//   {name}
+//   {age}
+//   {email}
+// </div>
+//   );
     return( <div className="App">
-    {user}
-    {user}
+    <User/>
+    <User/>  
      </div>
   );
 }
+
+const User = () => {
+  return (
+  <div>
+    <h1>Vanshaj</h1>
+    <h2>22</h2>
+    <h2>Abc@gmail.com</h2>
+  </div>
+  );
+};
 
 
 export default App;
