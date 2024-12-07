19c579737fa2a21f41611d439167e698713e2e28
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-07 23:48:04 +0530

    files

---
+++ src/components/topic2/Topic1.js
@@ -0,0 +1,73 @@
+import '../Topic1.css';
+
+// function App() {
+//   const name = "Vanshaj"
+//     return( <div className="App">
+//      <h1>{name}</h1>
+//      <h1>{name}</h1>
+//      <h1>{name}</h1>
+//      </div>
+//   );
+// }
+
+
+// function App() {
+//   const name = <h1>Vanshaj</h1>
+//   const age = <h2>21</h2>
+//   const email = <h2>Abc@gmail.com</h2>
+//   const user = ( 
+//   <div>
+//   {name}
+//   {age}
+//   {email}
+// </div>
+//   );
+//from here
+// return(
+//   <div className="App">
+//     <User name="Vanshaj" age={21} email="Abc@gmail.com"/>
+//     <User name="John" age={22} email="xyz@gmail.com"/>
+//     <User name="Arthur" age={20} email="acxy@gmail.com"/>
+//   </div>
+// );
+// }
+
+//what prop looks like
+// const props = {
+//   name: "Vanshaj",
+//   age: 22,
+//   email: "Abc@gmail.com"
+// }
+
+//using props to print multiple entries below this
+// const User = (props) =>{
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.age}</h2>
+//       <h2>{props.email}</h2>
+//     </div>
+//   );
+// }
+
+function Topic1(){
+  return (
+    <div className ="App">
+      <Job salary={10000} position="Senior SDE" company="Google"/>
+      <Job salary={20000} position="Junior SDE" company="Amazon"/>
+      <Job salary={30000} position="Project Head" company="Rupay"/>
+    </div>
+  );
+}
+
+const Job = (props) =>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+}
+
+export default Topic1;
---
+++ src/components/topic2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Topic1 from '../Topic1';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Topic1 />
+  </React.StrictMode>
+);
