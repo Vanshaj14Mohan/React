2ea7a167bb1c9f418db5ea22afd21d99c3540f95
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-07 20:16:29 +0530

    Exercise

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 // }
 
 
-function App() {
+// function App() {
 //   const name = <h1>Vanshaj</h1>
 //   const age = <h2>21</h2>
 //   const email = <h2>Abc@gmail.com</h2>
@@ -22,14 +22,15 @@ function App() {
 //   {email}
 // </div>
 //   );
-return(
-  <div className="App">
-    <User name="Vanshaj" age={21} email="Abc@gmail.com"/>
-    <User name="John" age={22} email="xyz@gmail.com"/>
-    <User name="Arthur" age={20} email="acxy@gmail.com"/>
-  </div>
-);
-}
+//from here
+// return(
+//   <div className="App">
+//     <User name="Vanshaj" age={21} email="Abc@gmail.com"/>
+//     <User name="John" age={22} email="xyz@gmail.com"/>
+//     <User name="Arthur" age={20} email="acxy@gmail.com"/>
+//   </div>
+// );
+// }
 
 //what prop looks like
 // const props = {
@@ -38,13 +39,33 @@ return(
 //   email: "Abc@gmail.com"
 // }
 
-//using props to print multiple entries
-const User = (props) =>{
+//using props to print multiple entries below this
+// const User = (props) =>{
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.age}</h2>
+//       <h2>{props.email}</h2>
+//     </div>
+//   );
+// }
+
+function App(){
   return (
+    <div className ="App">
+      <Job salary={10000} position="Senior SDE" company="Google"/>
+      <Job salary={20000} position="Junior SDE" company="Amazon"/>
+      <Job salary={30000} position="Project Head" company="Rupay"/>
+    </div>
+  );
+}
+
+const Job = (props) =>{
+  return(
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h2>{props.email}</h2>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
     </div>
   );
 }
